dde7ec40ec6e45e6e54011a40f5e6fc7560ed73f
Shubhan28 <83396802+Shubhan28@users.noreply.github.com>
2021-06-28 22:10:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ object_x = 0;
 object_y = 0;
 object = "";
 objects = [];
+alarm = "";
 function echo(message, another)
 {
     if(message)
@@ -16,6 +17,7 @@ function echo(message, another)
 function preload()
 {
     img = loadImage("image2.jpg");
+    alarm = loadSound("alarm.mp3");
 }
 function setup()
 {
@@ -23,8 +25,14 @@ function setup()
     canvas = createCanvas(640, 420);
     // We center() the canvas
     canvas.center();
+    // We start the video
+    video = createCapture(VIDEO);
+    // We hide the video
+    video.hide();
     // We connect our project to the cocossd model to help us
     classifier = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Baby!!"
+    echo("Detecting baby!!!");
 }
 function modelLoaded()
 {
@@ -39,25 +47,43 @@ function gotcocossd(error, results)
         console.error(error);
     } else {
        echo(results);
-       width = results[0].width;
-       height = results[0].height;
-       object_x = results[0].x;
-       object_y = results[0].y;
-       object = results[0].label;
-       objects = results;
        
     }
 }
 function draw()
 {
-    image(img, 0, 0, 640, 420);
-    for(i = 0; i > objects.length; i++)
+    image(video, 0, 0, 640, 420);
+    if(status != true)
     {
-        fill(255, 0, 255);
-        noFill();
-        stroke(0, 255, 255);
-        rect(objects[i].x, object[i].y, objects[i].width, objects[i].height);
-    }       
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        classifier.detect(video, gotcocossd);
+        alarm.stop();
+        for(z = 0; z > objects.length; z++)
+        {
+           document.getElementById("found").innerHTML = "Baby Found!!";
+           document.getElementById("status").innerHTML = "Status : Detected!!";
+           // We give the fill function
+           fill(r, g, b);
+           percent = floor(objects[z].confidence * 100);
+           text(objects[z].label + " " + percent + "%", objects[z].x + 15, objects[z].y + 15);
+           // We give the noFill() fucntion
+           noFill();
+           // We give the if condition
+           if(objects[z].label == "person")
+           {
+            text("Baby" + " " + percent + "%", objects[z].x + 15, objects[z].y + 15);
+           }
+           // We give the stroke function
+           stroke(r, g, b);
+           // We draw the rectangle
+           rect(objects[z].x, objects[z].y, objects[z].width, objects[z].height);
+        
+        }       
+    } else {
+        alarm.play();
+    }
+    
 }
-
 
